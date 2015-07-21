e6a757fb6382b4f70d257ce4d56eb2bdb6b2ad74
Chris (Yuan Qi) Ni <chris.ni@mail.utoronto.ca>
2015-07-21 17:08:14 -0400

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,7 @@
+class Data:
+  def __init__(self, data, pol, clean):
+        self.data = data        #npy array containing data
+        self.pol = pol          #(xx pol, yy pol)
+        self.clean = clean      #clean channels in data
+        
+  
