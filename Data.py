880179bb421cfa0f7dd6475d6a0271eb536f91a5
Chris (Yuan Qi) Ni <chris.ni@mail.utoronto.ca>
2015-07-21 18:17:57 -0400

    Update Data.py

--- Data.py
+++ Data.py
@@ -2,36 +2,39 @@ from numpy import*
 
 class Data:
   def __init__(self, data):
-        self.data = data        #npy array containing data
+    self.data = data        #npy array containing data
         
-  def findPulse(self, interval): #gets time bin interval around giant pulse
-        if len(self.data.shape) == 4:
-          print "Polarized data, assuming (0,3) (xx,yy) polarization"
-          Unpol.sumPols((0,3))
-        else:
-          Unpol = self
-        profile = Unpol.data.sum(1) #sum channels
-        giant = profile.argmax() #get maximum (giant pulse) bin
-        t = (giant - int(interval/2), giant + (interval/2)) #get time bin interval
-        return self.cropTime(t)
-
-    def cropTime(self, t_window): #operate on data, crop time axis
-        data = self.data[bstart:bend, ...]
-        return Data(data)
+  def findPulse(self): #gets time bin of giant pulse
+    if len(self.data.shape) == 4:
+      print "Polarized data, assuming (0,3) (xx,yy) polarization"
+      Unpol.sumPols((0,3))
+    else:
+      Unpol = self
+    profile = Unpol.data.sum(1) #sum channels
+    return profile.argmax() #get maximum (giant pulse) bin
     
-    def cropFreq(self, f_window): #operate on data, crop freq axis
-        data = self.data[:, bstart:bend, ...]
-        return Data(data)
+  def getPulseInterval(self, interval): #gets time bin interval around giant pulse
+    pulse = self.findPulse()
+    t = (giant - int(interval/2), giant + (interval/2)) #get time bin interval
+    return self.cropTime(t)
     
-    def sumPols(self, pols):
-        if len(self.data.shape) == 4:
-          data = self.data[..., pols].sum(-1)
-        else:
-          print "No polarizations to sum"
-          data = self.data
-        return Data(data)
+  def cropTime(self, t_window): #operate on data, crop time axis
+    data = self.data[bstart:bend, ...]
+    return Data(data)
     
-    def vlim(self): #get vmin,vmax colorbar limits
-        vmin = self.data.mean() - 1*self.data.std()
-        vmax = self.data.mean() + 1*self.data.std()
-        return vmin, vmax
+  def cropFreq(self, f_window): #operate on data, crop freq axis
+    data = self.data[:, bstart:bend, ...]
+    return Data(data)
+    
+  def sumPols(self, pols):
+    if len(self.data.shape) == 4:
+      data = self.data[..., pols].sum(-1)
+    else:
+      print "No polarizations to sum"
+      data = self.data
+    return Data(data)
+  
+  def vlim(self): #get vmin,vmax colorbar limits
+    vmin = self.data.mean() - 1*self.data.std()
+    vmax = self.data.mean() + 1*self.data.std()
+    return vmin, vmax
