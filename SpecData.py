8513ca7c4f1c091b93272214420e685a3ba6e301
Chris (Yuan Qi) Ni <chris.ni@mail.utoronto.ca>
2015-07-21 18:26:16 -0400

    Create SpecData.py

---
+++ SpecData.py
@@ -0,0 +1,56 @@
+import Data.py
+
+class SpecData(Data):
+  def __init__(self, data, t_all, f_all):
+    Data.__init__(self, data)
+    self.t_all = t_all
+    self.f_all = f_all
+    
+  def toTime(self, t_bin): #convert a bin to time units
+    interval = self.t_all[1] - self.t_all[0]
+    return self.t_all[0] + interval*t_bin/self.data.shape[0]
+    
+  def toTbin(self, time): #convert a time unit to bin
+    interval = self.t_all[1] - self.t_all[0]
+    return int((time - self.t_all[0])/interval)
+    
+  def toFreq(self, f_bin): #convert a chan to freq units
+    interval = self.f_all[1] - self.f_all[0]
+    return self.f_all[0] + interval*f_bin/self.data.shape[0]
+    
+  def toChan(self, freq): #convert a freq to channel
+    interval = self.f_all[1] - self.f_all[0]
+    return int((freq - self.f_all[0])/interval)
+    
+  def getPulseInterval(self, interval): #gets time bin interval around giant pulse
+    giant = Data.findPulse(self)
+    giant = self.toTbin(self)
+    t = (giant - interval/2, giant + interval/2) #get time bin interval
+    return self.cropTime(t)
+
+  def cropTime(self, t_window): #operate on data, crop time axis
+    interval = self.t_all[1] - self.t_all[0]
+    bstart = int(self.data.shape[0]*(t_window[0] - self.t_all[0])/interval)
+    bend = int(self.data.shape[0]*(t_window[1] - self.t_all[0])/interval)
+    data = self.data[bstart:bend, ...]
+    return SpecData(data, t_window, self.f_all)
+    
+  def cropFreq(f_window): #operate on data, crop freq axis
+    interval = self.f_all[1] - self.f_all[0]
+    bstart = int(self.data.shape[1]*(f_window[0] - self.f_all[0])/interval)
+    bend = int(self.data.shape[1]*(f_window[1] - self.f_all[0])/interval)
+    data = self.data[:, bstart:bend, ...]
+    return SpecData(data, self.t_all, f_window)
+    
+  def sumPols(self, pols):
+    if len(self.data.shape) == 4:
+      data = self.data[..., pols].sum(-1)
+    else:
+      print "No polarizations to sum"
+      data = self.data
+    return SpecData(data, self.t_all, self.f_all)
+    
+  def vlim(self): #get vmin,vmax colorbar limits
+    vmin = self.data.mean() - 1*self.data.std()
+    vmax = self.data.mean() + 1*self.data.std()
+    return vmin, vmax
