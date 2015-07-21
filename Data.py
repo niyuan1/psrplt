153ac92944de4511d0a2051b827b0fd791c1ed70
Chris (Yuan Qi) Ni <chris.ni@mail.utoronto.ca>
2015-07-21 17:50:06 -0400

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,18 +5,22 @@ class Data:
         self.data = data        #npy array containing data
         
   def findPulse(interval): #gets time bin interval around giant pulse
-        profile = profile.sum(1) #sum channels
+        profile = self.data.sum(1) #sum channels
         giant = profile.argmax() #get maximum (giant pulse) bin
         t = (giant - int(interval/2), giant + (interval/2)) #get time bin interval
         return self.cropTime(t)
 
     def cropTime(t_window): #operate on data, crop time axis
         data = data[bstart:bend, ...]
-        return Data(data, self.clean)
+        return Data(data)
     
     def cropFreq(f_window): #operate on data, crop freq axis
         data = data[:, bstart:bend, ...]
-        return Data(data, self.clean)
+        return Data(data)
+    
+    def sumPols(pols):
+        data = data.sum(-1)
+        return Data(data)
     
     def vlim(): #get vmin,vmax colorbar limits
         vmin = data.mean() - 1*data.std()
